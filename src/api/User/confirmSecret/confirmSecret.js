30a91b2af2690c9cf137a8206bfd651289859bf4
kuksu <kuksu95@hanmail.net>
2020-01-15 21:58:10 +0900

    #3.6

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -4,9 +4,10 @@ import { generateToken } from "../../../utils";
 export default {
     Mutation: {
         confirmSecret: async(_, args) => {
+            console.log(request);
             const { email, secret } = args;
             const user = await prisma.user({ email });
-            if(user.loginSecret === secret) {
+            if(user.loginSecret !== secret) {
                 return generateToken(user.id);
             }else {
                 throw Error("Wrong email/secret combination");
--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,6 +3,7 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
     Mutation: {
         createAccount: async (_, args) => {
+            console.log(prisma);
             const { name, email, firstName = "", lastName = "", bio = "" } = args;
             const user = await prisma.createUser({ name, email, firstName, lastName, bio });
             return user;
