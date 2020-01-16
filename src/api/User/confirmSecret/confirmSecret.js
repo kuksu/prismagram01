2da23be488c21756c9a974dba0fed59e600dad21
kuksu <kuksu95@hanmail.net>
2020-01-16 16:41:03 +0900

    #3.7

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -4,7 +4,6 @@ import { generateToken } from "../../../utils";
 export default {
     Mutation: {
         confirmSecret: async(_, args) => {
-            console.log(request);
             const { email, secret } = args;
             const user = await prisma.user({ email });
             if(user.loginSecret !== secret) {
