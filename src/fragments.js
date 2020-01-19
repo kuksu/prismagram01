365bb934abc1c3af293fb0c385e6d5b286e8bbc4
kuksu <kuksu95@hanmail.net>
2020-01-19 23:09:29 +0900

    #3.17

--- src/fragments.js
+++ src/fragments.js
@@ -1,9 +1,9 @@
 export const COMMENT_FRAGMENT = `
-fragment CommentParts on Comment {
-    id
-    text
-    user {
-        name
+    fragment CommentParts on Comment {
+        id
+        text
+        user {
+            name
+        }
     }
-}
 `
